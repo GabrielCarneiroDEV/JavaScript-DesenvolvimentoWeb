fd349444e993b5ef9c153d617c74f0c64043ba3d
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-06 19:57:55 -0300

    Update strings.js

---
+++ Fundamentos/strings.js
@@ -0,0 +1,7 @@
+// pode ser usado aspas duplas, aspas simples ou crase
+
+
+// a função charAt pode ser utilizada para mostrar um determinado caractere dentro da string. Assim como em uma array o indice tem inicio em 0. 
+const nome = "Gabriel"
+
+console.log(nome.charAt(0));
