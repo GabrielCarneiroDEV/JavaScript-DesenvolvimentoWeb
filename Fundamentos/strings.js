06ac645ffe72ce642f34350d6ac449bdd1caa84e
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-06 20:31:15 -0300

    Update strings.js

--- Fundamentos/strings.js
+++ Fundamentos/strings.js
@@ -5,3 +5,33 @@
 const nome = "Gabriel"
 
 console.log(nome.charAt(0));
+
+
+// a função charCodeAt busca o código relacionado ao caractere na tabela unicode:
+
+console.log(nome.charCodeAt(0));
+
+// a função indexOf retorna a posição de determinado caractere dentro do indice
+
+console.log(nome.indexOf("G"))
+
+
+// substring pode determinar o ponto em que a string é exibida. Também pode ser indicado o final.
+console.log(nome.substring(0, 3))
+
+// concatenação 
+
+console.log("Olá ".concat(nome).concat("!"))
+
+//outra forma de concatenar
+
+console.log ("Olá " + nome + "!")
+
+// replace pode modificar um caractere ou vários dentro da string
+console.log(nome.replace("Gabriel", "Gabzin"))
+
+// split converter string em array a partir de um ponto determinado pode ser um espaço, virgula, ponto, caractere...
+
+console.log("João, Maria, josé".split(","))
+
+
