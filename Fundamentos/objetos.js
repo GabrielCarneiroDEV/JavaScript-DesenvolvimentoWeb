a59cbd39a7238a457f82c71660858532a8687db1
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-07 19:25:00 -0300

    Create objetos.js

---
+++ Fundamentos/objetos.js
@@ -0,0 +1,21 @@
+// em js um objeto é representado por chaves {}, enquanto uma array por colchetes[]
+
+
+const prod1 = {}
+prod1.nome = "notebook barriu dobrado"
+prod1.preço = 300.000
+
+
+
+console.log (prod1)
+
+//outra forma de escrever um objeto 
+const pessoa={
+    nome: "jose",
+    idade: 18,
+    sexo: "M"
+    }
+    console.log(pessoa)
+
+// não confundir com "json" - javascript objective notation 
+//json é um formato de objeto textual usado para comunicação entre sistemas variados   
