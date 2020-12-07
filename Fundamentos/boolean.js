285cfcefdf89bf967f9fc8e87f4aba2891fa6656
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-06 23:18:37 -0300

    Create boolean.js

---
+++ Fundamentos/boolean.js
@@ -0,0 +1,21 @@
+let sOuN = false
+
+console.log(sOuN)
+
+
+sOuN = true
+
+console.log(sOuN)
+
+// também pode ser representado por 1 ou 0 pra isso é necessário usar a negação "!", nesse caso é utilizado negação dupla pra que o resultado não seja oposto. 
+
+
+//verdadeiro
+console.log(!! sOuN)
+
+//falso
+console.log(! sOuN)
+
+//os valores inteiros positivos ou negativos sempre se comportam como true e zero como falso
+
+
