ec4e0809042d1652bb36dcada6f61adc1de1e7e6
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-07 20:17:18 -0300

    Create função em toda parte.js

---
+++ Fundamentos/função em toda parte.js
@@ -0,0 +1,11 @@
+//a função é o principal conceito dentro do Java Script
+
+//console.log(typeof Object)
+// até mesmo os objects são funções
+
+
+
+class Produto {}
+console.log(typeof Produto)
+
+//uma class também é um tipo de função em JS
