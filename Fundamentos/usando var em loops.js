7988dd4d020694b8897a5af457eb21fa9aeaa529
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-08 10:24:19 -0300

    Update usando var em loops.js

--- Fundamentos/usando var em loops.js
+++ Fundamentos/usando var em loops.js
@@ -1,7 +1,14 @@
-
+//mais um exemplo da utilização de var e let, nesse caso utilizando a estrutura de repetição for. No caso do var, é possível configurar a saída fora da estrutura de repetição, no let, não. 
 
 
 
 for (var i = 0; i <=10; i++){
     console.log(i)
 }
+console.log("i = ", i)
+
+for (let j = 0; j <=10; j++){
+    console.log(j)
+}
+console.log("j = ", j)
+
