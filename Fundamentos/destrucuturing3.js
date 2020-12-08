3226b7834da1fbcc5b9ae901ace125549d3c5691
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-08 17:54:19 -0300

    Create destrucuturing3.js

---
+++ Fundamentos/destrucuturing3.js
@@ -0,0 +1,17 @@
+//usando destructuring como parametro de uma função
+
+function rand ({min =0, max = 1000}){
+    const valor = Math.random() * (max-min) + min
+    return Math.floor(valor)
+}
+
+
+//passando parametro da função
+const obj = {max: 100, min:0}
+
+
+
+//sorteando 10 numeros usando for
+for (let i = 0; i<=10; i++){
+    console.log(rand(obj))
+}
