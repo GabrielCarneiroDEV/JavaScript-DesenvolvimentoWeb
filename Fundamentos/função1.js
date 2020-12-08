5c49a4173dd7735a7f1223110cc7e65e948f59da
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-07 21:09:24 -0300

    Create função1.js

---
+++ Fundamentos/função1.js
@@ -0,0 +1,25 @@
+//função sem retorno
+
+function helloworld(){
+    console.log("hello world!")
+    
+}
+
+helloworld();
+
+function soma(a,b){
+    console.log(a+b)
+}
+
+soma(2,4);
+soma("hello ", "World!");
+
+
+//função com retorno
+
+function soma2(a,b){
+return a+b
+
+}
+
+console.log(soma2(2,3))
