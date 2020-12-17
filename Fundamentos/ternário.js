30e2163ee292833cca037466d8978113e8db494f
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-17 18:59:53 -0300

    Create ternário.js

---
+++ Fundamentos/ternário.js
@@ -0,0 +1,10 @@
+//função arrow junto com operador ternário
+
+
+const resultado = nota =>{
+    return nota >= 7 ? 'aprovado' : 'reprovado';
+}
+console.log(resultado (7.1))
+console.log (resultado(6.7))
+
+
