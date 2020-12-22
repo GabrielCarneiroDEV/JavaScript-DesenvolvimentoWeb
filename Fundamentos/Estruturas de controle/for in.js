d5f5dab83dd141396e58574d37400e8d7bfae60f
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-22 20:53:52 -0300

    Create for in.js

---
+++ Fundamentos/Estruturas de controle/for in.js
@@ -0,0 +1,16 @@
+const notas= [-2, 6.6, 0, 20, 30]
+ for(i in notas)
+ {
+     console.log(`${notas[i]}`)
+ }
+
+const pessoa = {
+nome:'Gabriel',
+idade: '26',
+bonito: 'sim', 
+}
+for (i in pessoa){
+    console.log(`${i} ${pessoa[i]}`)
+}
+
+ //maneira mais fácil de percorrer uma array ou objeto
