9c0505ce4a8a00faab46565ae1a4921c34de8619
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-26 18:57:34 -0300

    Update primeiraclasse.js

--- Funções/primeiraclasse.js
+++ Funções/primeiraclasse.js
@@ -19,3 +19,23 @@ console.log(array1[0](2,3))
 
 //a função tbm pode ser armazenada em um atributo de um objeto
 const obj = {}
+obj.falar = function () {return 'fala tu'}
+console.log(obj.falar())
+
+//passar função como parametro par aoutra função
+
+function run (func1){
+    func1()
+}
+
+run(function () {console.log('executando')})
+
+//uma função pode retornar/conter uma função
+
+function soma (a,b){
+    return function (c){
+        console.log(a+b+c)
+    }
+}
+
+soma(2,2)(2)
